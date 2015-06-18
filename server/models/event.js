c71f7b9ef2979fe1c66c95cec7bc75f68a00478e
futbalguy <kyle.rokita@gmail.com>
2015-06-18 16:37:01 -0700

    added user and event model/collections

---
+++ server/models/event.js
@@ -0,0 +1,20 @@
+var db = require('../db-config');
+var User = require('./user')
+
+var Event = db.Model.extend({
+  tableName: 'events',
+  hasTimestamps: false,  //CAN CHANGE THIS LATER, ALSO UPDATE DB-CONFIG IF SO
+  // defaults: {
+  //   visits: 0
+  // },
+  initialize: function(){
+    // this.on('creating', function(model, attrs, options){
+
+    // });
+  },
+  user: function() {
+    return this.belongsTo(User, 'user_id');
+  }
+});
+
+module.exports = Event;
