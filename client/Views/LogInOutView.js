357f51d340c15cf156a8475d42566f324b174981
Alberto d'Souza <ardsouza1@gmail.com>
2015-06-18 19:15:44 -0700

    fixed some shit

---
+++ client/Views/LogInOutView.js
@@ -0,0 +1,26 @@
+app.logInOutView = Backbone.View.extend({
+  id : 'loginout',
+
+  initialize : function() {
+    //this.logoutView = new LogoutView({});
+    //this.loginView = new LoginView({});
+  },
+
+  events : {
+    'app_loggedInState' : 'viewLogout', //login trigger
+    'app_loggedOutState' : 'viewLogin', //logout trigger
+  },
+
+  viewLogin : function() {
+    this.render('loginView');
+  },
+
+  viewLogout : function() {
+    this.render('logoutView');
+  },
+
+  render : function(view) {
+    this.$el.children().detach();
+    this.$el.append(this[view].render());
+  }
+});
