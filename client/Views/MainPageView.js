b6ddd00004eaf92f8b22b82d7fbfa09e2b380a20
Alberto d'Souza <ardsouza1@gmail.com>
2015-06-22 15:15:10 -0700

    added some organizational views

---
+++ client/Views/MainPageView.js
@@ -0,0 +1,13 @@
+app.mainPageView = Backbone.View.extend({
+  // className : 'loginout',
+  el: document.getElementsByClassName('col-md-8'),
+
+  initialize : function() {
+  },
+
+  events : {
+  },
+
+  render : function() {
+  }
+});
