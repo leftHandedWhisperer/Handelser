39d45589a93cbd8e60f46301428d737d6d939040
Jammie Mountz <jmmountz@gmail.com>
2015-06-24 16:39:34 -0700

    integrated mocha, chai, travisCI, and heroku deploy

---
+++ tests/tests.js
@@ -0,0 +1,24 @@
+// var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+ 
+describe("Application", function() {
+  it("creates a global variable for the name space", function () {
+    should.exist(app);
+  })
+  it('provides an "app" object', function(){
+    testApp = new app();
+    expect(testApp).to.be.an('object');
+  });
+  it('has the global views on the object', function() {
+    should.exist(app.calendarView);
+    should.exist(app.dayView);
+    should.exist(app.navbarView);
+    should.exist(app.mainPageView);
+    should.exist(app.sidePageView);
+  })
+})
+
+describe("Calendar View", function() {
+  
+})
