a3557c28b8ec557dd00e5bcda056672ce000b5b5
Jammie Mountz <jmmountz@gmail.com>
2015-06-24 17:07:46 -0700

    commenting out tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,23 +2,23 @@
 var expect = chai.expect;
 var should = chai.should();
  
-describe("Application", function() {
-  it("creates a global variable for the name space", function () {
-    should.exist(app);
-  })
-  it('provides an "app" object', function(){
-    testApp = new app();
-    expect(testApp).to.be.an('object');
-  });
-  it('has the global views on the object', function() {
-    should.exist(app.calendarView);
-    should.exist(app.dayView);
-    should.exist(app.navbarView);
-    should.exist(app.mainPageView);
-    should.exist(app.sidePageView);
-  })
-})
+// describe("Application", function() {
+//   it("creates a global variable for the name space", function () {
+//     should.exist(app);
+//   })
+//   it('provides an "app" object', function(){
+//     testApp = new app();
+//     expect(testApp).to.be.an('object');
+//   });
+//   it('has the global views on the object', function() {
+//     should.exist(app.calendarView);
+//     should.exist(app.dayView);
+//     should.exist(app.navbarView);
+//     should.exist(app.mainPageView);
+//     should.exist(app.sidePageView);
+//   })
+// })
 
-describe("Calendar View", function() {
+// describe("Calendar View", function() {
   
-})
+// })
