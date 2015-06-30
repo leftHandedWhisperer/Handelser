68fdea030a31d1ba2fe70600dcb5c4ae323a58f9
Matthew Altschuler Jacobs <Matt@Matthews-MacBook-Pro-3.local>
2015-06-29 20:00:45 -0700

    Added comments

--- client/Collections/Events.js
+++ client/Collections/Events.js
@@ -5,6 +5,7 @@ app.Events = Backbone.Collection.extend({
     if (!options) {
       options = {};
     }
+    //app.filteredEvents refers to the events that are currently visible to the client
     options.success = function(data) {
        app.filteredEvents.reset(data.models);
     }
